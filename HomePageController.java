960043df489e6039536cde51a5294da50fd87a16
AndrewAran <32881698+AndrewAran@users.noreply.github.com>
2018-04-17 16:34:02 -0400

    Add files via upload

--- HomePageController.java
+++ HomePageController.java
@@ -1,9 +1,14 @@
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import javafx.scene.control.Button;
@@ -14,58 +19,191 @@ import java.util.ResourceBundle;
 
 public class HomePageController implements Initializable {
 
+    @FXML
+    public Button BackButton;
 
-    public void HandleEB(ActionEvent e){
-        try{
+    @FXML
+    public Button HomeButton;
 
-            Parent UserFrame = FXMLLoader.load(getClass().getResource("EventsPage.fxml"));
-            Scene UserFrameScene = new Scene(UserFrame);
-            Stage getUserFrame = (Stage) ((Node) e.getSource()).getScene().getWindow();
-            getUserFrame.setScene(UserFrameScene);
-            getUserFrame.show();
+    @FXML
+    public Button AnnouncementButton;
 
-        }catch(IOException e1){
-            System.out.println(e);
-        }
+    @FXML
+    public Button EventsButton;
 
-    }
+    @FXML
+    public Button GroupsButton;
 
-    public void HandleHBA(ActionEvent actionEvent) {
+    @FXML
+    public Button UserListButton;
 
-        try{//this will allow the user to essentially refresh the page since they are already on the home page
-            Parent UserFrame = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-            Scene UserFrameScene = new Scene(UserFrame);
-            Stage getUserFrame = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            getUserFrame.setScene(UserFrameScene);
-            getUserFrame.show();
+    @FXML
+    public Button LogoutButton;
 
-        }catch(IOException e){//Catches Io exception and prints out the error to the console.
-            System.out.println(e);
-        }
-    }
+    @FXML
+    public Button ProfileButton;
 
-    public void HandleAB(ActionEvent actionEvent) {
+    @FXML
+    public Label Label1;
 
-    }
+    @FXML
+    public Label Label2;
 
-    public void HandlePB(ActionEvent actionEvent) {
+    @FXML
+    public Label Label3;
 
-    }
+    @FXML
+    public Label Label4;
 
-    public void HandleGB(ActionEvent actionEvent) {
-
-    }
-
-    public void HandleULB(ActionEvent actionEvent) {
-
-    }
-
-    public void HandleLOB(ActionEvent actionEvent) {
-
-    }
+    @FXML
+    public Label Label5;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        HomeButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
 
+        EventsButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("EventsPage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        AnnouncementButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("AnnouncementsPage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        ProfileButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        GroupsButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("GroupsPage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        UserListButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("UserListPage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        LogoutButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                Stage popupwindow = new Stage();
+
+                popupwindow.initModality(Modality.APPLICATION_MODAL);
+                popupwindow.setTitle("Log Out Confirmation");
+
+                Label label1 = new Label("Are you sure you want to log out?");
+
+                Button button1 = new Button("Yes");
+                Button button2 = new Button("No");
+
+                VBox layout= new VBox(10);
+
+
+                layout.getChildren().addAll(label1, button1, button2);
+                layout.setAlignment(Pos.CENTER);
+                Scene scene1= new Scene(layout, 300, 250);
+                popupwindow.setScene(scene1);
+                popupwindow.showAndWait();
+
+                button2.setOnAction(event1 -> popupwindow.close());
+                button1.setOnAction(new EventHandler<>() {
+
+                    @Override
+                    public void handle(javafx.event.ActionEvent event) {
+                        try {
+                            Parent UserFrame = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                            Scene UserFrameScene = new Scene(UserFrame);
+                            Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                            getUserFrame.setScene(UserFrameScene);
+                            getUserFrame.show();
+                        } catch (IOException e) {
+                            System.out.println(e);
+                        }
+                    }
+                });
+            }
+        });
+
+        BackButton.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                try {
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                    Scene UserFrameScene = new Scene(UserFrame);
+                    Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    getUserFrame.setScene(UserFrameScene);
+                    getUserFrame.show();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        });
     }
 }
