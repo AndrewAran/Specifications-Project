15155c7f5c6fa34f2c8e35e91bd98e967ee1869b
AndrewAran <32881698+AndrewAran@users.noreply.github.com>
2018-04-18 18:35:39 -0400

    Add files via upload

--- CreateUserAccountController.java
+++ CreateUserAccountController.java
@@ -52,13 +52,13 @@ public class CreateUserAccountController implements Initializable {
                     System.out.println(e);
                 }
             }
-        });
+    });
 
         CButton.setOnAction(new javafx.event.EventHandler<>() {
             @Override
             public void handle(javafx.event.ActionEvent event) {//This needs to have the database stuff to add the new user to the user list, currently it just brings you to the home page
                 try {
-                    Parent UserFrame = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+                    Parent UserFrame = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
                     Scene UserFrameScene = new Scene(UserFrame);
                     Stage getUserFrame = (Stage) ((Node) event.getSource()).getScene().getWindow();
                     getUserFrame.setScene(UserFrameScene);
