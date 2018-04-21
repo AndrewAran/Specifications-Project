fa60ff79c497dd19277b721ef0dbafd38bac003a
bjsimmons0221 <37807475+bjsimmons0221@users.noreply.github.com>
2018-04-21 13:29:57 -0400

    Add files via upload

--- LogInController.java
+++ LogInController.java
@@ -2,6 +2,10 @@
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,7 +20,7 @@ import javafx.stage.Stage;
 public class LogInController implements Initializable {
 
     @FXML
-    public TextField UNTextField;
+    public TextField UserNameTextField;
     public static boolean isAdmin = false;
 
     public void HandleCreateButtonAction(ActionEvent event){//currently it just goes to appropriate page
@@ -30,8 +34,32 @@ public class LogInController implements Initializable {
             System.out.println(e);
         }
     }
-    public void HandleLogInButtonAction(ActionEvent event) {//this happens when login button is pressed. here we will put password validation
-        if(UNTextField.getText().equals("Andrew Aran")){
+    public void HandleLogInButtonAction(ActionEvent event) throws Exception{//this happens when login button is pressed. here we will put password validation
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/H2Test");
+        Statement stat = conn.createStatement();
+        ResultSet rs1;
+        ResultSet rs2;
+        String temp;
+        rs1 = stat.executeQuery("select username from account");
+        while (rs1.next()) {
+            if(UserNameTextField.getText().equals(rs1.getString("username"))){
+
+                System.out.println(rs1.getString("username"));
+                temp = rs1.getString("username");
+
+                rs2 = stat.executeQuery("select password from account");
+                while (rs2.next()) {
+
+                    System.out.println(rs2.getString("password"));
+                }
+                break;
+
+            }
+
+            System.out.println(rs1.getString("username"));
+        }
+        if(UserNameTextField.getText().equals("Andrew Aran")){
             isAdmin = true;
         }
 
